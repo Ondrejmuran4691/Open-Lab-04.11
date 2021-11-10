a2947623a32130cb1b44f138dfb9fa0133704129
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-11-10 12:24:44 +0100

    puts letters in string to alphabetic order

---
+++ Open-Lab-04.11/Open-Lab-04.11/StringTools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Open_Lab_04._11
+{
+    public class StringTools
+    {
+        public string AlphabetSoup(string str)
+        {
+
+
+            char[] done = str.ToArray();
+            Array.Sort(done);
+            string n = "";
+                foreach (char c in done)
+            {
+                n += c.ToString();
+            }
+            return n;
+
+
+
+        }
+    }
+}
